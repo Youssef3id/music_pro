2b21213b2e7703a431379c469fd2e21774992150
Youssef Mohammed <youssef.3id@icloud.com>
2025-02-19 18:15:04 +0200

    add: added route

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Home from '@/views/HomeView.vue'
+import About from '@/views/AboutView.vue'
+import Manage from '@/views/Manage.vue'
+import useUserStore from '@/stores/user'
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+  {
+    path: '/manage',
+    component: Manage,
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: '/',
+  },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes,
+  linkExactActiveClass: 'text-yellow-500',
+})
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next()
+    return
+  }
+  const store = useUserStore()
+  if (store.userLoggedIn) {
+    next()
+  } else {
+    next('/')
+  }
 })
 
 export default router
--- src/stores/user.js
+++ src/stores/user.js
@@ -36,6 +36,7 @@ export default defineStore('user', {
     async signOut() {
       await auth.signOut()
       this.userLoggedIn = false
+      window.location.reload()
     },
   },
 })
